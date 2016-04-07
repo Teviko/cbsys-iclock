9c7c5ffcf81a35d6358e4a1ee0359c52722b8931
Albert Zhang <albert.zhang@gmail.com>
2016-04-07 11:01:22 +0800

    remove test method

--- src/main/java/com/cbsys/iclock/utils/ClockUtils.java
+++ src/main/java/com/cbsys/iclock/utils/ClockUtils.java
@@ -6,10 +6,7 @@ import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
 import java.time.zone.ZoneRules;
-import java.util.Locale;
-import java.util.Set;
 import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
@@ -62,26 +59,26 @@ public class ClockUtils {
 		return OffsetDateTime.ofInstant(Instant.ofEpochMilli(utcTime), ZoneOffset.of(tz)).format(FormatterToTMS);
 	}
 
-	public static void main(String[] args) {
-		//	String[] ids = TimeZone.getAvailableIDs();
-		//	System.out.println(ids.length);
-		//	System.out.println(TimeZone.getTimeZone("NZ").getOffset(System.currentTimeMillis()) / 1000 / 60 / 60);
-		Set<String> zoneIds = ZoneId.getAvailableZoneIds();
-		System.out.println(zoneIds.size());
-		//System.out.println(ZoneId.of("NZ").getRules().getOffset(Instant.parse("2016-03-02T15:56:57z")).getTotalSeconds() / 60 / 60);
-		for (String zoneId : zoneIds) {
-			ZoneId z = ZoneId.of(zoneId);
-			System.out.println(zoneId + "," + z.getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "," + z.getRules().toString().split("=")[1].replace("]", ""));
-		}
-		//for (String id : ids)
-		//	System.out.println(id);
-		//	System.out.println(OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-		//Date time = format.parse("2016-03-02T15:56:57+1300");
-		long t = 1458573700000l;
-		long t2 = transToUTC("2016-04-06 15:56:57", "NZ")[0];
-		//	System.out.println(t);
-		//	System.out.println(t2);
-		//	System.out.println(Instant.ofEpochMilli(t2).toString());
-		//	System.out.println(transToOffsetDatetime(t, 13));
-	}
+	/*	public static void main(String[] args) {
+			//	String[] ids = TimeZone.getAvailableIDs();
+			//	System.out.println(ids.length);
+			//	System.out.println(TimeZone.getTimeZone("NZ").getOffset(System.currentTimeMillis()) / 1000 / 60 / 60);
+			Set<String> zoneIds = ZoneId.getAvailableZoneIds();
+			System.out.println(zoneIds.size());
+			//System.out.println(ZoneId.of("NZ").getRules().getOffset(Instant.parse("2016-03-02T15:56:57z")).getTotalSeconds() / 60 / 60);
+			for (String zoneId : zoneIds) {
+				ZoneId z = ZoneId.of(zoneId);
+				System.out.println(zoneId + "," + z.getDisplayName(TextStyle.FULL, Locale.ENGLISH) + "," + z.getRules().toString().split("=")[1].replace("]", ""));
+			}
+			//for (String id : ids)
+			//	System.out.println(id);
+			//	System.out.println(OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+			//Date time = format.parse("2016-03-02T15:56:57+1300");
+			long t = 1458573700000l;
+			long t2 = transToUTC("2016-04-06 15:56:57", "NZ")[0];
+			//	System.out.println(t);
+			//	System.out.println(t2);
+			//	System.out.println(Instant.ofEpochMilli(t2).toString());
+			//	System.out.println(transToOffsetDatetime(t, 13));
+		}*/
 }
