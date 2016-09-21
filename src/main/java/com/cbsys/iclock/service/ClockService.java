aaba04756a7227451c0a6b75d2ecc9057af02009
Albert Zhang <albert.zhang@gmail.com>
2016-09-21 17:03:32 +0800

    no message

--- src/main/java/com/cbsys/iclock/service/ClockService.java
+++ src/main/java/com/cbsys/iclock/service/ClockService.java
@@ -377,8 +377,9 @@ public class ClockService {
 		}*/
 	}
 
-	private static final String TMS_URL = "http://114.23.34.60:9090/import/pr?token=";
+	private static final String TMS_URL = "http://45.32.242.201:9090/import/pr?token=";
 
+	//114.23.34.60
 	public void syncAttRecords() throws IOException, ParseException {
 		List<AttRecord> attRecords = attRecordDao.findTop100BySyncFlag(0);
 		if (CollectionUtils.isEmpty(attRecords))
